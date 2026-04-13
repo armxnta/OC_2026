e20bba32ae79fb4ac690c6fc6f06ab2e1257760d
armxnta <Luis.armenta94@uabc.edu.mx>
2026-04-13 10:24:25 +0000

    guardar cambios

--- src/practica6/p6.asm
+++ src/practica6/p6.asm
@@ -1,9 +1,8 @@
-%include "../../lib/pc_io.inc"  	; incluir declaraciones de procedimiento externos
-								; que se encuentran en la biblioteca libpc_io.a
+%include "../../lib/pc_io.inc"
 
-section	.text
-	global _start       ;referencia para inicio de programa
-	
+section .text
+global _start
+
 _start:                   
 
     call getch
@@ -11,34 +10,48 @@ _start:
     mov edx,ncad
     call puts
 
-    
-
     mov bx,word[len]
     mov edx,cad
     call capturar
+
+    mov al,[nlin]
+    call putchar
+    call puts        ; imprime original
+
+    ; mayusculas
+    mov edx,cad
+    call mayusculas
+
     mov al,[nlin]
     call putchar
     call puts
 
+    ; minusculas
+    mov edx,cad
+    call minusculas
+
     mov al,[nlin]
     call putchar
-    call putchar
+    call puts
 
-	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
-	int	0x80        	; llamada al sistema - fin de programa
+    mov eax,1
+    int 0x80
+
 
     capturar:
         push edx
         push cx
         mov cx,bx
         dec cx
+
     .ciclo: 
         call getch
         cmp al,127
         jne .guardar
         call borrar
         jmp .ciclo
-       .guardar:
+
+    .guardar:
         call putchar
         mov [edx],al
         cmp al,0xa
@@ -46,11 +59,60 @@ _start:
         inc edx
         loop .ciclo
 
-        .salir:
+    .salir:
         mov byte[edx],0
         pop cx
         pop edx
         ret
+
+    mayusculas:
+        push edx
+
+    .loop:
+        mov al,[edx]
+        cmp al,0
+        je .fin
+
+        cmp al,'a'
+        jb .siguiente
+        cmp al,'z'
+        ja .siguiente
+
+        sub al,32
+        mov [edx],al
+
+    .siguiente:
+        inc edx
+        jmp .loop
+
+    .fin:
+        pop edx
+        ret
+
+    minusculas:
+        push edx
+
+    .loop:
+        mov al,[edx]
+        cmp al,0
+        je .fin
+
+        cmp al,'A'
+        jb .siguiente
+        cmp al,'Z'
+        ja .siguiente
+
+        add al,32
+        mov [edx],al
+
+    .siguiente:
+        inc edx
+        jmp .loop
+
+    .fin:
+        pop edx
+        ret
+
 
     borrar:
         push ax 
@@ -74,13 +136,12 @@ _start:
         mov al,ah
         add al,'0'
         call putchar
-
-
-
         pop bx
         ret
-section	.data
-    ncad db 0xa,'Cadena: ',0
-    nlin db 0xa
-    len db 64
-    cad	times 64 db 0
+
+
+    section .data
+        ncad db 0xa,'Cadena: ',0
+        nlin db 0xa
+        len db 64
+        cad times 64 db 0
